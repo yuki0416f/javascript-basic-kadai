816c9df6d8e6233e3d10979240d913776de80b61
yuki0416f <ydk60ynee85p@gmail.com>
2024-07-29 16:16:10 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+
+for (let num = 1; num <= 100; num++) {
+
+// 変数numの値が3と5の倍数なら、3と5の倍数です
+if(num % 15 === 0) {
+    console.log('3と５の倍数です');
+}
+
+// 変数numの値が3の倍数なら、３の倍数です
+else if(num % 3 === 0) {
+    console.log('3の倍数です');
+}
+// 変数numの値が5の倍数なら、5の倍数です
+else if(num % 5 === 0) {
+    console.log('5の倍数です');
+}
+else{
+    console.log('num')
+}
+}
