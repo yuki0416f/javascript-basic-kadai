ca4a53c54658fb5a5e271a0d2a0ab2619f957ef1
yuki0416f <ydk60ynee85p@gmail.com>
2024-07-31 14:12:17 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,8 @@
-var today = new Date("August, 22,2023");
-console.log(today)
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth()+1,
+  date.getDate(),
+  date.getFullYear(),
+];
+
+console.log(year+"年"+month+"月"+day+"日");
